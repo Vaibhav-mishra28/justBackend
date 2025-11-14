827dce00bf3fbd7df9f728ff9f90c2dbcc8b5d07
Vaibhav <vms282004@gmail.com>
2025-11-14 23:11:38 +0530

    added utils

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
+
+
+const connectDB = async ()=>{
+    try{
+         const connectionInstance = await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);  //connection Instance ko ek baar print krake dekhna
+    } catch(error){
+        console.error("MONGODB connection error", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB
